044fc759147d1e34e58910f7cd14dacdef116978
Tomáš Jukin <tomas@jukin.cz>
2010-01-06 08:56:52 +0100

    Favorites BL have been reviewed, commented and finalized.

--- Kesinek/KesinekBusinessLayer/src/java/kesinek/businesslayer/session/FavoritesBean.java
+++ Kesinek/KesinekBusinessLayer/src/java/kesinek/businesslayer/session/FavoritesBean.java
@@ -1,46 +1,76 @@
 /*
  * This file describes Favorites management session bean class.
+ *
+ * - reviewed: 6. 1. 2010, 7:55
+ * - finalized: 6. 1. 2010, 7:55
+ *
+ * @author Tomáš Jukin
  */
 
 package kesinek.businesslayer.session;
 
 import java.util.Collection;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import kesinek.businesslayer.entities.Favorite;
+import kesinek.businesslayer.entities.IsInFavorite;
 import kesinek.businesslayer.entities.ProductItem;
 
-/**
- * Handles BL for Favorites and ProductItem entity classes
- *
- * This bean will perform basic I/O operations with product (EC ProductItem) in the favorite lists (EC Favorite)
- *
- * Favorite lists are kind of system wide stats mechanism
- *
- * @author Tomáš Jukin
- */
 @Stateless
 public class FavoritesBean implements FavoritesBeanLocal {
 
+    @PersistenceContext
+    private EntityManager em;
+
     public void addProductToFavorites(ProductItem product, Favorite favorites) {
+        em.persist(new IsInFavorite().setFavoriteID(favorites).setProductItemID(product));
     }
 
     public void removeProductFromFavorites(ProductItem product, Favorite favorites) {
+        IsInFavorite relation = em.merge(new IsInFavorite().setFavoriteID(favorites).setProductItemID(product));
+        em.remove(relation);
     }
 
-    /**
-     * Will increment desired product evaluation in desired favorite list
-     * 
-     * @param product
-     * @param favorites
-     */
     public void incrementProductEvaluation(ProductItem product, Favorite favorites) {
+        em.createNamedQuery("IsInFavorite.updateAmount")
+                .setParameter("productItemID", product.getProductItemID())
+                .setParameter("favoriteID", favorites.getFavoritesID())
+                .setParameter("amount", ((Integer)em.createNamedQuery("IsInFavorite.findAmount")
+                    .setParameter("productItemID", product.getProductItemID())
+                    .setParameter("favoriteID", favorites.getFavoritesID())
+                .getSingleResult() + 1)
+        )
+        .executeUpdate();
     }
 
     public void decrementProductEvaluation(ProductItem product, Favorite favorites) {
+        em.createNamedQuery("IsInFavorite.updateAmount")
+                .setParameter("productItemID", product.getProductItemID())
+                .setParameter("favoriteID", favorites.getFavoritesID())
+                .setParameter("amount", ((Integer)em.createNamedQuery("IsInFavorite.findAmount")
+                    .setParameter("productItemID", product.getProductItemID())
+                    .setParameter("favoriteID", favorites.getFavoritesID())
+                .getSingleResult() - 1)
+        )
+        .executeUpdate();
     }
 
-    public Collection<ProductItem> getMostFavoriteProducts(Favorite favorite) {
-        return null;
+    @SuppressWarnings("unchecked")
+    public Collection<ProductItem> getMostFavoriteProducts(Favorite favorite, Integer number) {
+        return em.createNamedQuery("Favorite.findMostFavoriteProducts")
+                .setParameter("favoritesID", favorite.getFavoritesID())
+                .setParameter("limit", number)
+        .getResultList();
+    }
+
+    public void addFavorite(Favorite favorites) {
+        em.persist(favorites);
+    }
+
+    public void removeFavorite(Favorite favorites) {
+        favorites = em.merge(favorites);
+        em.remove(favorites);
     }
  
 }
--- Kesinek/KesinekBusinessLayer/src/java/kesinek/businesslayer/session/FavoritesBeanLocal.java
+++ Kesinek/KesinekBusinessLayer/src/java/kesinek/businesslayer/session/FavoritesBeanLocal.java
@@ -1,5 +1,6 @@
 /*
  * This file describes Favorites management session bean local interface.
+ * @author Tomáš Jukin
  */
 
 package kesinek.businesslayer.session;
@@ -12,19 +13,71 @@ import kesinek.businesslayer.entities.ProductItem;
 /**
  * Handles BL for Favorites and ProductItem entity classes
  *
+ * This bean will perform basic I/O operations with product (EC ProductItem) in the favorite lists (EC Favorite)
+ *
+ * Favorite lists are kind of system wide stats mechanism
+ *
+ * - reviewed: 6. 1. 2010, 7:55
+ * - finalized: 6. 1. 2010, 7:55
+ *
  * @author Tomáš Jukin
  */
 @Local
 public interface FavoritesBeanLocal {
 
+    /**
+     * Will an event to the system
+     *
+     * @param favorites
+     */
+    void addFavorite(Favorite favorites);
+
+    /**
+     * Will remove desired event from the system
+     *
+     * @param favorites
+     */
+    void removeFavorite(Favorite favorites);
+
+    /**
+     * Will add desired product to desired favorite list
+     *
+     * @param product
+     * @param favorites
+     */
     void addProductToFavorites(ProductItem product, Favorite favorites);
 
+    /**
+     * Will remove desired product from desired favorite list
+     *
+     * @param product
+     * @param favorites
+     */
     void removeProductFromFavorites(ProductItem product, Favorite favorites);
 
+    /**
+     * Will increment desired product evaluation in desired favorite list
+     *
+     * @param product
+     * @param favorites
+     */
     void incrementProductEvaluation(ProductItem product, Favorite favorites);
 
+    /**
+     * Will increment desired product evaluation in desired favorite list
+     *
+     * @param product
+     * @param favorites
+     */
     void decrementProductEvaluation(ProductItem product, Favorite favorites);
 
-    Collection<ProductItem> getMostFavoriteProducts(Favorite favorite);
+    /**
+     * Will list of most favorite products in given favorite
+     *
+     * @param favorite
+     * @param number How many product do you want to have?
+     * @return Collection<ProductItem>
+     */
+    Collection<ProductItem> getMostFavoriteProducts(Favorite favorite, Integer number);
     
 }
